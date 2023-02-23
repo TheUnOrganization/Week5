9074094af4a7b5c0e2e160755a5f18c0313daaf0
Brendan Moor <bmoor9027@students.mc3.edu>
2023-02-23 00:57:17 -0500

    Week 5 Lab Part 2

--- Week5/my-app/src/App.js
+++ Week5/my-app/src/App.js
@@ -1,15 +1,50 @@
-import Add from './components/Add';
-import Subtract from './components/Subtract';
-import Multiply from './components/Multiply';
-import Divide from './components/Divide';
+import { useState } from 'react';
+
+import Calculation from './components/Calculation';
+import Result from './components/Result';
 
 function App() {
+  const [enteredNumbers, setEnteredNumbers] = useState({ first: 0, second: 0 });
+  const [chosenOperation, setChosenOperation] = useState('add'); // valid values: 'add', 'subtract', 'multiply', 'divide'
+
+  function changeFirstNumberHandler(event) {
+    setEnteredNumbers((prevNumbers) => ({
+      first: +event.target.value,
+      second: prevNumbers.second,
+    }));
+  }
+
+  function changeSecondNumberHandler(event) {
+    setEnteredNumbers((prevNumbers) => ({
+      first: prevNumbers.first,
+      second: +event.target.value,
+    }));
+  }
+
+  function updateOperationHandler(event) {
+    setChosenOperation(event.target.value);
+  }
+
+  let result;
+
+  if (chosenOperation === 'add') {
+    result = enteredNumbers.first + enteredNumbers.second;
+  } else if (chosenOperation === 'subtract') {
+    result = enteredNumbers.first - enteredNumbers.second;
+  } else if (chosenOperation === 'multiply') {
+    result = enteredNumbers.first * enteredNumbers.second;
+  } else {
+    result = enteredNumbers.first / enteredNumbers.second;
+  }
+
   return (
     <>
-      <Add />
-      <Subtract />
-      <Multiply />
-      <Divide />
+      <Calculation
+        onFirstNumberChanged={changeFirstNumberHandler}
+        onSecondNumberChanged={changeSecondNumberHandler}
+        onOperationChanged={updateOperationHandler}
+      />
+      <Result calculationResult={result} />
     </>
   );
 }
