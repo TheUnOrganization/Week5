59aa4d1f38ae23843ef6aad54c90ba72f25de596
Brendan Moor <bmoor9027@students.mc3.edu>
2023-02-23 00:51:26 -0500

    Week 5 Lab Part 1

---
+++ Week5/my-app/src/App.js
@@ -0,0 +1,17 @@
+import Add from './components/Add';
+import Subtract from './components/Subtract';
+import Multiply from './components/Multiply';
+import Divide from './components/Divide';
+
+function App() {
+  return (
+    <>
+      <Add />
+      <Subtract />
+      <Multiply />
+      <Divide />
+    </>
+  );
+}
+
+export default App;
